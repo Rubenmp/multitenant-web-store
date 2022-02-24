25dbb07857a055eef3a0ca5e3f3220235d2cea4d
Ruben <srmorales@correo.ugr.es>
2022-02-24 02:12:29 +0100

    Rename methods

--- code/front-end/src/app/app.component.ts
+++ code/front-end/src/app/app.component.ts
@@ -7,27 +7,15 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
-  sideMenuAction!: string;
 
   @ViewChild("sideMenu") sideMenu!: ElementRef;
 
   openSideMenu() {
-    console.log("openSideMenu");
-    this.sideMenuAction = "OPEN";
-  }
-
-
-  openNav() {
-    console.log("openNav")
-    console.log(this.sideMenu);
-
     this.sideMenu.nativeElement.style.width = "250px";
     this.sideMenu.nativeElement.style.marginLeft = "0px";
   }
 
-  closeNav() {
-    console.log("closeNav")
-
+  closeSideMenu() {
     this.sideMenu.nativeElement.style.width = "0";
     this.sideMenu.nativeElement.style.marginLeft= "0";
   }
