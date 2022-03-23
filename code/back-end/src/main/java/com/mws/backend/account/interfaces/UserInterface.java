1ec4b879238c9a7bec198d306032559c136c8068
Ruben <srmorales@correo.ugr.es>
2022-03-23 20:47:31 +0100

    Move endpoint

--- code/back-end/src/main/java/com/mws/backend/account/interfaces/UserInterface.java
+++ code/back-end/src/main/java/com/mws/backend/account/interfaces/UserInterface.java
@@ -1,4 +1,4 @@
-package com.mws.backend.account;
+package com.mws.backend.account.interfaces;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
