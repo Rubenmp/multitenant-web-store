ab15fdbee5502e71813f3c8256e6ee24156fbd10
Ruben <srmorales@correo.ugr.es>
2022-03-26 01:35:33 +0100

    Simplify constructor

--- code/back-end/src/main/java/com/mws/backend/framework/dto/WebResult.java
+++ code/back-end/src/main/java/com/mws/backend/framework/dto/WebResult.java
@@ -1,32 +1,21 @@
 package com.mws.backend.framework.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class WebResult<DataClass extends Serializable> {
     private WebResultCode code;
     private String message;
     private DataClass data;
 
-/*
-    public static WebResult success(final String message) {
-        WebResult result = new WebResult();
-        result.setCode(WebResultCode.SUCCESS);
-        result.setMessage(message);
-        return result;
-    }*/
-
     public static <E extends Serializable> WebResult<E> success(final E data) {
-        WebResult<E> result = new WebResult<E>();
-        result.setCode(WebResultCode.SUCCESS);
-        //result.setMessage(message);
-        result.setData(data);
-
-        return result;
+        return new WebResult<>(WebResultCode.SUCCESS, null, data);
     }
 
 }
