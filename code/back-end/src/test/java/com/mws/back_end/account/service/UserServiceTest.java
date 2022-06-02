175cf1c3adbaa39e6d48e713900756629fdcb6da
Ruben <srmorales@correo.ugr.es>
2022-06-02 02:12:37 +0200

    Fix unit tests for user services

--- code/back-end/src/test/java/com/mws/back_end/account/service/UserServiceTest.java
+++ code/back-end/src/test/java/com/mws/back_end/account/service/UserServiceTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -32,6 +33,9 @@ class UserServiceTest extends TestUtils {
 
     @Mock
     private TenantService tenantService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserService userService;
