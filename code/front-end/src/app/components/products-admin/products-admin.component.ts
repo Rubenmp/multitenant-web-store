9b002b5f856f68ec98b84be984c4d32dea2c2a8a
Ruben <rbnmp@protonmail.com>
2022-09-03 07:21:59 +0200

    Basic structure for product management

--- code/front-end/src/app/components/products-admin/products-admin.component.ts
+++ code/front-end/src/app/components/products-admin/products-admin.component.ts
@@ -7,6 +7,10 @@ import { NotificationService } from 'src/service/notification/notification.servi
 import { Product } from 'src/service/product/dto/product';
 import { ProductsService } from 'src/service/product/products.service';
 
+
+type TenantRow = { tenantId: number, name: string, active: boolean, isBeingUpdated: boolean, isSelected: boolean }
+type TenantCreation = { name: string }
+
 @Component({
   selector: 'app-products-admin',
   templateUrl: './products-admin.component.html',
@@ -26,6 +30,18 @@ export class ProductsAdminComponent implements OnInit {
 
   filterById: string = "";
   filterByProductName: string = "";
+
+  // Creation
+  tenantToCreate: TenantCreation[] = [];
+  inputTenantName: string = '';
+  isCreatingTenant: boolean = false;
+  displayedColumnsToCreate: string[] = ['name'];
+  dataSourceToCreate!: MatTableDataSource<TenantCreation>;
+
+  // Update/delete
+  selectedRows: number = 0;
+  isBeingUpdated: boolean = false;
+
 
 
   constructor(private productsService: ProductsService,
@@ -51,6 +67,10 @@ export class ProductsAdminComponent implements OnInit {
         this.notificationService.showError(e.error.message);
       },
     });
+
+    this.isCreatingTenant = false;
+    this.isBeingUpdated = false;
+    this.selectedRows = 0;
   }
 
 
@@ -85,4 +105,120 @@ export class ProductsAdminComponent implements OnInit {
     const filterValue = (event.target as HTMLInputElement).value;
     return filterValue.trim().toLowerCase();
   }
+
+
+
+
+
+
+
+
+
+
+  // TODO
+  clickRow(row: TenantRow) {
+    if (this.isBeingUpdated) {
+      return;
+    }
+    if (row.isSelected) {
+      row.isSelected = false;
+      this.selectedRows -= 1;
+    } else {
+      row.isSelected = true;
+      this.selectedRows += 1;
+    }
+  }
+
+  isRowSelected() {
+    return this.selectedRows > 0;
+  }
+
+  // Tenants operations
+  async create() {
+    /*
+    if (this.isCreatingTenant) {
+      await (await this.productsService.createTenant(this.inputTenantName)).subscribe({
+        next: async (response) => {
+          if (isOkResponse(response)) {
+            this.notificationService.showInfoMessage("Tenant created with id " + response.data);
+            
+            this.fetchProducts();
+          } else {
+            this.notificationService.showError(response.message);
+          }
+        },
+        error: (e) => {
+          this.notificationService.showError(e.error.message);
+        },
+      });
+
+    } else {
+      this.isCreatingTenant = true;
+      this.tenantToCreate = [{ name: '' }]
+      this.dataSourceToCreate = new MatTableDataSource(this.tenantToCreate);
+    }
+    */
+  }
+
+
+  async cancelAction() {
+    await this.fetchProducts();
+    if (this.paginator && !this.paginator.pageSize) {
+      this.paginator.pageSize = 5;
+    }
+  }
+
+
+  async update() {
+    /*
+    if (this.selectedRows > 0) {
+      const rowsToUpdate = this.products.filter(row => row.isSelected);
+      const isUpdating = (rowsToUpdate.length > 0 && rowsToUpdate[0].isBeingUpdated);
+      if (isUpdating) {
+        for (let rowToUpdate of rowsToUpdate) {
+          await (await this.productsService.updateTenant(rowToUpdate.tenantId, rowToUpdate.name)).subscribe({
+            next: (response) => {
+              if (isOkResponse(response)) {
+                this.notificationService.showInfoMessage("Tenant updated");
+              } else {
+                this.notificationService.showError(response.message);
+              }
+            },
+            error: (e) => {
+              this.notificationService.showError(e.error.message);
+            },
+          });
+        }
+        await this.fetchProducts();
+      
+      } else {
+        rowsToUpdate.forEach(row => row.isBeingUpdated = true);
+        this.isBeingUpdated = true;
+      }
+    }*/
+  }
+
+
+  async deleteSelected() {/*
+    if (this.selectedRows > 0) {
+      const selectedTenantIds = this.products.filter(row => row.isSelected).map(tenant => tenant.tenantId);
+
+      for (let selectedTenantId of selectedTenantIds) {
+        await (await this.productsService.delete(selectedTenantId)).subscribe({
+          next: async (response) => {
+            if (isOkResponse(response)) {
+              this.notificationService.showInfoMessage("Tenant deleted");
+              await this.fetchProducts();
+            } else {
+              this.notificationService.showError(response.message);
+            }
+          },
+          error: (e) => {
+            this.notificationService.showError(e.error.message);
+          },
+        });
+      }
+    }*/
+  }
+
 }
